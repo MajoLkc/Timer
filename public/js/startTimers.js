dd2bfd471d8edc866443926d1181ec5b01fcd99a
firemnyOscar <firemnyoscar@gmail.com>
2020-10-01 12:26:56 +0200

    Added reseb button (not working correctly)

--- public/js/startTimers.js
+++ public/js/startTimers.js
@@ -1,8 +1,22 @@
-function getSelectedTimers() {
-  let checkbox, timersSelected = [];
-  const numberOfTimers = document.querySelectorAll('.col').length;
+let timerIndex;
+const numberOfTimers = document.querySelectorAll('.col').length;
 
-  for (var numberOfTimer = 1; numberOfTimer <= numberOfTimers; numberOfTimer++) {
+function getAllNumbers(id) {
+  var numbers = [];
+  for (timerIndex = 0; timerIndex < numberOfTimers; timerIndex++) {
+    numbers.push(Number(document.querySelector(id + timerIndex).textContent));
+  }
+  return numbers;
+};
+
+const staticVariables = {
+  numbersOfMinutes: getAllNumbers('#minutes-'),
+  numbersOfSeconds: getAllNumbers('#seconds-')
+};
+
+function getSelectedTimers() {
+  let checkbox, numberOfTimer, timersSelected = [];
+  for (numberOfTimer = 0; numberOfTimer < numberOfTimers; numberOfTimer++) {
     checkbox = document.querySelector('#timerCheckbox-' + numberOfTimer).checked;
     if (checkbox) {
       timersSelected.push(numberOfTimer);
@@ -11,22 +25,74 @@ function getSelectedTimers() {
   return timersSelected;
 }
 
+function selectNumberFromId(id) {
+  var str = id;
+  var splitId = str.split("-");
+  var number = splitId[1];
+  return number;
+}
+
+function resetCountdown(buttonId) {
+  buttonId = selectNumberFromId(buttonId);
+  const progress = document.querySelector('#progress-done-' + buttonId);
+  const minutesPosition = document.getElementById('minutes-' + buttonId);
+  const secondsPosition = document.getElementById('seconds-' + buttonId);
+  const numberOfMinutes = staticVariables.numbersOfMinutes[buttonId];
+  const numberOfSeconds = staticVariables.numbersOfSeconds[buttonId];
+  const timer = document.querySelector('#timer-' + buttonId);
+  const timerDoneText = document.querySelector('#timerDone-' + buttonId);
+  const resetBtn = document.querySelector('#resetBtn-' + buttonId);
+  let seconds = (60 * numberOfMinutes) + numberOfSeconds;
+  const fullSeconds = seconds;
+
+  setInterval(function () {
+    let minutes = Math.round((seconds - 30) / 60);
+    let remainingSeconds = seconds % 60;
+    let remainingPercentage = (seconds / fullSeconds) * 100;
+
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    // add 0 if there is less then 10 seconds
+    if (remainingSeconds < 10) {
+      remainingSeconds = '0' + remainingSeconds;
+    }
+
+    // stop counting at 0
+    if (seconds <= 0) {
+      clearInterval(remainingSeconds = 0);
+      clearInterval(minutes = 0);
+      progress.style.width = 0 + '%';
+      timer.style.display = 'none';
+      timerDoneText.innerHTML = 'Time left! You should do...';
+    } else { //display current time
+      minutesPosition.innerHTML = minutes;
+      secondsPosition.innerHTML = remainingSeconds;
+      progress.style.width = remainingPercentage + '%';
+      seconds--;
+    }
+  }, 1000)
+}
+
 function countdown() {
   let timerNumber, timersSelected;
   timersSelected = getSelectedTimers();
   var numberOfSelectedTimers = timersSelected.length;
 
   for (timerNumber = 0; timerNumber < numberOfSelectedTimers; timerNumber++) {
-
     const progress = document.querySelector('#progress-done-' + timersSelected[timerNumber]);
     const minutesPosition = document.getElementById('minutes-' + timersSelected[timerNumber]);
     const secondsPosition = document.getElementById('seconds-' + timersSelected[timerNumber]);
-    const numberOfMinutes = Number(document.getElementById('minutes-' + timersSelected[timerNumber]).textContent);
-    const numberOfSeconds = Number(document.getElementById('seconds-' + timersSelected[timerNumber]).textContent);
+    const numberOfMinutes = staticVariables.numbersOfMinutes[timersSelected[timerNumber]];
+    const numberOfSeconds = staticVariables.numbersOfSeconds[timersSelected[timerNumber]];
     const timer = document.querySelector('#timer-' + timersSelected[timerNumber]);
     const timerDoneText = document.querySelector('#timerDone-' + timersSelected[timerNumber]);
+    const resetBtn = document.querySelector('#resetBtn-' + timersSelected[timerNumber]);
     let seconds = (60 * numberOfMinutes) + numberOfSeconds;
     const fullSeconds = seconds;
+
+    resetBtn.style.display = 'block';
 
     setInterval(function () {
       let minutes = Math.round((seconds - 30) / 60);
@@ -60,13 +126,7 @@ function countdown() {
 }
 
 
-
-// enableButton(startBtn);
-
 document.addEventListener('DOMContentLoaded', () => {
   const startBtn = document.querySelector('#turnOnTimers');
-
-  countdown();
-
   startBtn.addEventListener('click', countdown);
 })
